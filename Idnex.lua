55380387be288cf4129c91e4070fe9d2c023632f
.index <81807841+twhlynch@users.noreply.github.com>
2024-12-30 23:36:59 +1100

    bump version

--- Idnex.lua
+++ Idnex.lua
@@ -4,7 +4,7 @@
 --- MOD_AUTHOR: [.index]
 --- MOD_DESCRIPTION: A custom joker and deck
 --- BADGE_COLOUR: 00FFFF
---- VERSION: 0.1.0
+--- VERSION: 0.2.0
 --- PREFIX: idnex
 
 ----------------------------------------------
